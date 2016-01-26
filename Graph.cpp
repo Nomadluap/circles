77c96e9aa4a309625c167f2fc41d751eef014434
Paul Belanger <paulirino@gmail.com>
2016-01-25 22:49:49 -0500

    Added Circles namespace, almost done implementing Circles::Graph

--- Graph.cpp
+++ Graph.cpp
@@ -1,7 +1,91 @@
 #include "Graph.hpp"
+using namespace Circles;
 
-Graph::Graph()
+Circles::Graph::Graph(): // default constructor
+    edges(new QHash<Node, QSet<Node> >()),
+    boundaryNodes(new QSet<Node>())
 {
+    //nothing here.
 
+}
+
+Circles::Graph::Graph(const Graph& other): // copy constructor
+    edges(new QHash<Node, QSet<Node> >(*other.edges)),
+    boundaryNodes(new QSet<Node>(*other.boundaryNodes))
+{
+    //nothing here either boss.
+}
+
+Circles::Graph::Graph(Graph&& other): // move constructor
+    edges(std::move(other.edges)),
+    boundaryNodes(std::move(other.boundaryNodes))
+{
+    //nothing to do here.
+}
+
+Graph& Circles::Graph::operator=(const Graph& other) // copy assignment
+{
+    edges = std::make_unique<QHash<Node, QSet<Node> > >(*other.edges);
+    boundaryNodes = std::make_unique<QSet<Node> >(*other.boundaryNodes);
+    return *this;
+}
+
+Graph& Circles::Graph::operator=(Graph&& other) // move assignment
+{
+    edges = std::move(other.edges);
+    boundaryNodes = std::move(other.boundaryNodes);
+    return *this;
+}
+
+
+void Circles::Graph::addEdge(Node x, Node y)
+{
+    if(x == y) return; // no self-edges allowed.
+
+    // we need to add y to the edge set of x, and x to the edge set of y to maintain symmetry
+    if(!this->edges->contains(x)) this->edges->insert(x, QSet<Node>());
+    if(!this->edges->contains(y)) this->edges->insert(y, QSet<Node>());
+
+    auto xset = this->edges->take(x);
+    xset.insert(y);
+    this->edges->insert(x, std::move(xset));
+
+    auto yset = this->edges->take(y);
+    yset.insert(x);
+    this->edges->insert(y, std::move(yset));
+
+}
+
+void Circles::Graph::removeEdge(Node x, Node y)
+{
+    if(!this->hasEdge(x, y)) return;
+
+    // the edge exists and must be terminated. (Read in arnold Schwarzenegger voice)
+    auto xset = this->edges->take(x);
+    xset.remove(y);
+    this->edges->insert(x, std::move(xset));
+
+    auto yset = this->edges->take(y);
+    yset.remove(x);
+    this->edges->insert(y, std::move(yset));
+
+}
+
+bool Circles::Graph::hasEdge(Node x, Node y)
+{
+    if(!this->edges->contains(x) || !this->edges->contains(y)) return false; // edge does not exist since nodes do not exist.
+    else if(!this->edges->value(x).contains(y) || !this->edges->value(y).contains(x)) return false; // edge not defined.
+    else return true; //otherwise teh edge exists.
+}
+
+std::unique_ptr<QList<Node> > Circles::Graph::getNodes()
+{
+    auto nodes = std::make_unique<QList<Node> >(this->edges->keys());
+    return nodes;
+}
+
+std::unique_ptr<QList<Edge> > Circles::Graph::getEdges()
+{
+    //TODO: implement this.
 }
 
--- Graph.hpp
+++ Graph.hpp
@@ -3,24 +3,66 @@
 
 #include <QHash>
 #include <QSet>
+#include <memory>
 
-/**
- * Implements a mathematical graph.
- */
-class Graph
-{
-public:
-    Graph();
-    Graph(const Graph& other); //copy constructor
-    Graph& operator=(const Graph& other); //assignment operator
+namespace Circles{
+    //typedefs to simplify the language.
+    typedef QPair<int, int> Edge;
+    typedef int Node;
 
-    void addEdge(int x, int y);
-    void removeEdge(int x, int y);
-    bool hasEdge(int x, int y);
+    /**
+     * Implements a mathematical graph.
+     * The graph also stores information about the boundary nodes and edges.
+     */
+    class Graph
+    {
+    public:
+        Graph();
+        Graph(const Graph& other); //copy constructor
+        Graph(Graph&& other); //move constructor
+        Graph& operator=(const Graph& other); //assignment operator
+        Graph& operator=(Graph&& other); //move assignment
 
-private:
-    QHash<int, QSet<int> > edges;
-    QSet<int> boundaryNodes;
-};
+        /**
+         * Add an edge to the graph. Note that edges are symmetric. eg. AB == BA
+         * @param x index of one end of the edge
+         * @param y indexc of the other end of the edge.
+         */
+        void addEdge(Node x, Node y);
+
+        /**
+         * Removes an edge from the graph, if it exists.
+         * @param x index of first node which defines edge
+         * @param y index of second node which defines edge
+         */
+        void removeEdge(Node x, Node y);
+
+        /**
+         * Deterimine if the graph contains the edge between two nodes.
+         * @param x the first node
+         * @param y the second node
+         * @return True if the edge is contained in the graph, otherwise false.
+         */
+        bool hasEdge(Node x, Node y);
+
+        /**
+         * Get the set of nodes that are present in the graph.
+         * @return Set of the nodes present in the graph
+         */
+        std::unique_ptr<QList<Node> > getNodes();
+
+        /**
+         *  Get a set of all edges in the graph.
+         * The pairs returned will always have the lowest-order node index first.
+         * @return A set of node pairs representing the edges of the graph.
+         */
+        std::unique_ptr<QList<Edge> > getEdges();
+
+    private:
+        std::unique_ptr<QHash<Node, QSet<Node> > > edges;
+        std::unique_ptr<QSet<Node> > boundaryNodes;
+    };
+
+}
 
 #endif // GRAPH_HPP
